a0fd8e57a547fefd0c5a0282e1f7f094f36a1811
George Broadley <gbroadley96@gmail.com>
2016-10-29 17:25:05 +0100

    Created migrations for the site

--- database/migrations/2016_10_29_162044_create_users_table.php
+++ database/migrations/2016_10_29_162044_create_users_table.php
@@ -15,11 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
+            $table->integer('role_id');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+
+            $table->foreign('role_id')->references('id')->on('roles');
         });
     }
 
---
+++ database/migrations/2016_10_29_165533_create_jokes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJokesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jokes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('joke');
+            $table->string('punchline');
+            $table->boolean('flagged');
+            $table->string('category_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jokes');
+    }
+}
